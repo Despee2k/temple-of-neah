1a6d1ce5a9d4e37d3b862fa8d7d3897a6951b31a
Dempsey <dagantareynat@gmail.com>
2025-12-16 03:10:55 +0800

    merge: resolve conflicts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,23 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< Updated upstream
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
-=======
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
->>>>>>> Stashed changes
 });
