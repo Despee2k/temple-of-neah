f4e445633f2d8a9005325314ffe3e836e78b2bef
Ron Patrick Ramas <143059096+MrChooey@users.noreply.github.com>
2025-12-16 12:18:47 +0800

    feat: update styles and configurations for dark mode, enhance .gitignore files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,12 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+const rootElement = document.documentElement;
+if (!rootElement.classList.contains("dark")) {
+	rootElement.classList.add("dark");
+}
+rootElement.style.colorScheme = "dark";
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
